ccfef64722e72ef2767a043b0e9eb6017eae1ece
Juan Blanco <juanfranblanco@hotmail.com>
2021-03-07 07:27:52 +0000

    Adding server side to complete options, although this should be just for local usage. With accounts etc

---
+++ NethereumExplorer.Server/Startup.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NethereumExplorer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazor.FlexGrid;
+using NethereumExplorer.ViewModels;
+
+namespace NethereumExplorer.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            //NOTE: Server side singleton, just for local usage, you would not want to use the Account services in server mode
+            /// use the Metamask integration in other templates
+            
+            var web3ServiceProvider = new Web3ProviderService();
+            var accountsService = new AccountsService(web3ServiceProvider);
+            var newBlockProcessingService = new NewBlockProcessingService(web3ServiceProvider);
+            var toastsViewModel = new ToastsViewModel();
+            var blocksViewModel = new BlocksViewModel(newBlockProcessingService);
+            var latestBlockTransactionsViewModel = new LatestBlockTransactionsViewModel(web3ServiceProvider);
+            var newAccountPrivateKeyViewModel = new NewAccountPrivateKeyViewModel();
+            var accountsViewModel = new AccountsViewModel(accountsService, newAccountPrivateKeyViewModel);
+            var accountsTransactionMonitoringService = new AccountsTransactionMonitoringService(accountsService, web3ServiceProvider);
+
+            services.AddSingleton<IWeb3ProviderService, Web3ProviderService>((x) => web3ServiceProvider);
+            services.AddSingleton<IAccountsService, AccountsService>((x) => accountsService);
+            services.AddSingleton<NewBlockProcessingService>(newBlockProcessingService);
+            services.AddSingleton<ToastsViewModel>(toastsViewModel);
+            services.AddSingleton<BlocksViewModel>(blocksViewModel);
+            services.AddSingleton<LatestBlockTransactionsViewModel>(latestBlockTransactionsViewModel);
+            services.AddTransient<BlockTransactionsViewModel>();
+            services.AddSingleton<AccountsViewModel>(accountsViewModel);
+            services.AddSingleton<NewAccountPrivateKeyViewModel>(newAccountPrivateKeyViewModel);
+            services.AddSingleton<SendTransactionViewModel>();
+            services.AddSingleton<SendErc20TransactionViewModel>();
+            services.AddSingleton(accountsTransactionMonitoringService);
+            services.AddSingleton<TransactionWithReceiptViewModel>();
+
+            services.AddFlexGridServerSide(cfg =>
+            {
+                cfg.ApplyConfiguration(new TransactionsViewModelGridConfiguration());
+            });
+
+            services.AddSingleton<Web3UrlViewModel>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
