afb65f57d4e432193500c2b52e82ee6b9a27fcdc
Juan Blanco <juanfranblanco@hotmail.com>
2021-11-09 18:00:36 +0000

    Update NethereumExplorer.Core to include supporting frameworks and updates to maui

--- NethereumExplorer.Maui/Platforms/Android/MainApplication.cs
+++ NethereumExplorer.Maui/Platforms/Android/MainApplication.cs
@@ -1,19 +1,19 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.Runtime;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
-using System;
 
 namespace NethereumExplorer.Maui
 {
-    [Application]
-    public class MainApplication : MauiApplication
-    {
-        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-            : base(handle, ownership)
-        {
-        }
+	[Application]
+	public class MainApplication : MauiApplication
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+			: base(handle, ownership)
+		{
+		}
 
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    }
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
 }
