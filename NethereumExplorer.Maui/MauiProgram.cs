a5b29a30f28fd9d9efe2f792fe1a4ebc63a1506e
Miguel Ángel Barrera <mbarrera@plainconcepts.com>
2022-04-30 11:03:31 +0200

    migrate RC2

--- NethereumExplorer.Maui/MauiProgram.cs
+++ NethereumExplorer.Maui/MauiProgram.cs
@@ -1,66 +1,19 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Maui;
-using Microsoft.Maui.Hosting;
-using Microsoft.Maui.Controls.Compatibility;
-using Microsoft.Maui.Controls.Hosting;
-using NethereumExplorer.Services;
-using NethereumExplorer.ViewModels;
-using Blazor.FlexGrid;
-using System.Net.Http;
+﻿namespace NethereumExplorer.Maui;
 
-namespace NethereumExplorer.Maui
+public static class MauiProgram
 {
-	public static class MauiProgram
-	{
-		public static MauiApp CreateMauiApp()
-		{
-            
-            var builder = MauiApp.CreateBuilder();
-			builder
-				.RegisterBlazorMauiWebView()
-				.UseMauiApp<App>()
-				.ConfigureFonts(fonts =>
-				{
-					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				});
+    public static MauiApp CreateMauiApp()
+    {
 
-			builder.Services.AddBlazorWebView();
- 
-            var services = builder.Services;
-            
-            var web3ServiceProvider = new Web3ProviderService();
-            var accountsService = new AccountsService(web3ServiceProvider);
-            var newBlockProcessingService = new NewBlockProcessingService(web3ServiceProvider);
-            var toastsViewModel = new ToastsViewModel();
-            var blocksViewModel = new BlocksViewModel(newBlockProcessingService);
-            var latestBlockTransactionsViewModel = new LatestBlockTransactionsViewModel(web3ServiceProvider);
-            var newAccountPrivateKeyViewModel = new NewAccountPrivateKeyViewModel();
-            var accountsViewModel = new AccountsViewModel(accountsService, newAccountPrivateKeyViewModel);
-            var accountsTransactionMonitoringService = new AccountsTransactionMonitoringService(accountsService, web3ServiceProvider);
-
-            services.AddSingleton<IWeb3ProviderService, Web3ProviderService>((x) => web3ServiceProvider);
-            services.AddSingleton<IAccountsService, AccountsService>((x) => accountsService);
-            services.AddSingleton<NewBlockProcessingService>(newBlockProcessingService);
-            services.AddSingleton<ToastsViewModel>(toastsViewModel);
-            services.AddSingleton<BlocksViewModel>(blocksViewModel);
-            services.AddSingleton<LatestBlockTransactionsViewModel>(latestBlockTransactionsViewModel);
-            services.AddTransient<BlockTransactionsViewModel>();
-            services.AddSingleton<AccountsViewModel>(accountsViewModel);
-            services.AddSingleton<NewAccountPrivateKeyViewModel>(newAccountPrivateKeyViewModel);
-            services.AddSingleton<SendTransactionViewModel>();
-            services.AddSingleton<SendErc20TransactionViewModel>();
-            services.AddSingleton(accountsTransactionMonitoringService);
-            services.AddSingleton<TransactionWithReceiptViewModel>();
-            services.AddSingleton<Web3UrlViewModel>();        
-           
-
-            services.AddFlexGrid(cfg =>
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
             {
-                cfg.ApplyConfiguration(new TransactionsViewModelGridConfiguration());
-            });
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            })
+            .ConfigureServices();
 
-            return builder.Build();
-        }
-	}
+        return builder.Build();
+    }
 }
