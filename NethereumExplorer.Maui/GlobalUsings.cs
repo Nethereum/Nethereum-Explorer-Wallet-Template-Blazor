57e0ce858c8683956742fd2856a292e4a36b1af8
Miguel Ángel Barrera <mbarrera@plainconcepts.com>
2022-04-30 11:03:31 +0200

    migrate RC2

---
+++ NethereumExplorer.Maui/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using Microsoft.Maui;
+global using Microsoft.Maui.Controls;
+global using Microsoft.Maui.Hosting;
+global using Microsoft.Maui.Controls.Hosting;
+global using Blazor.FlexGrid;
+global using Microsoft.Extensions.DependencyInjection;
+global using NethereumExplorer.Services;
+global using NethereumExplorer.ViewModels;
